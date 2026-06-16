e050af1be635688ca424d46fa8d4b6389a4be811
lbestudante-hash <lbestudante@gmail.com>
2026-06-16 16:31:55 -0300

    barboza

--- components/hero.tsx
+++ components/hero.tsx
@@ -5,6 +5,7 @@ import Image from 'next/image'
 import { motion } from 'motion/react'
 import { GoldParticles } from './gold-particles'
 import { whatsappLink } from '@/lib/site'
+import { Calendar, MessageCircle } from 'lucide-react'
 
 
 
